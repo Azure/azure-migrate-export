337af12acaaa263035c15ba511de6267bf1b8f03
Harsh Rathore <harshrathore@microsoft.com>
2023-03-01 23:32:30 +0530

    Removed revision

--- .version/GenerateVersion.ps1
+++ .version/GenerateVersion.ps1
@@ -1,9 +1,10 @@
+[CmdletBinding()]
+
 $major = "2"
 $minor = "0"
-$patch = Get-Date -Format "yyMMdd"
-$revision = $Env:BUILD_BUILDNUMBER
+$patch = $Env:BUILD_BUILDNUMBER
 
-$buildNumber = "$major.$minor.$patch.$revision"
+$buildNumber = "$major.$minor.$patch"
 Write-Host = $buildNumber
 [Environment]::SetEnvironmentVariable("CustomBuildNumber", $buildNumber, "User")  # This will allow you to use it from env var in later steps of the same phase
 Write-Host "##vso[build.updatebuildnumber]${buildNumber}"                         # This will update build number on your build
