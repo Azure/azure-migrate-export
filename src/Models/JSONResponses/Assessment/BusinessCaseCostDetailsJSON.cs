e1369501a5ed18d187602b3ade73da0cf4da64c3
Ishaan Marwah <ishaanmarwah@microsoft.com>
2024-10-11 18:06:52 +0530

    update: resolving errors

--- src/Models/JSONResponses/Assessment/BusinessCaseCostDetailsJSON.cs
+++ src/Models/JSONResponses/Assessment/BusinessCaseCostDetailsJSON.cs
@@ -24,5 +24,8 @@ namespace Azure.Migrate.Export.Models
 
         [JsonProperty("securityCost")]
         public double? SecurityCost { get; set; }
+
+        [JsonProperty("facilitiesCost")]
+        public double? FacilitiesCost { get; set; }
     }
 }
