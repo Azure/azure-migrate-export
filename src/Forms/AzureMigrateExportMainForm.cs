c2c1fd5c59724d23048a401b456f2d7f411308c9
Ishaan Marwah <ishaanmarwah@microsoft.com>
2024-11-15 12:50:43 +0530

    Revert "handling the initializaiton of the assessment settings form on change in the configuration form"
    This reverts commit 5a46c7f377e224f3928d107a44ff1b1b330604ca.

--- src/Forms/AzureMigrateExportMainForm.cs
+++ src/Forms/AzureMigrateExportMainForm.cs
@@ -189,9 +189,6 @@ namespace Azure.Migrate.Export.Forms
                 nextButtonTab.BackColor = Color.FromArgb(120, 190, 255);
                 CurrentButtonTab = nextButtonTab;
             }
-            if(nextButtonTab == AssessmentSettingsTabButton){
-                AssessmentSettingsFormObj.Initialize();
-            }
 
             OpenChildForm(formObjToActivate);
         }
